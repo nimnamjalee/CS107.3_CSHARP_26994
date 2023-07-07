a38375ca7c57c5e0293729f5e9e57940d3f2e4b2
Kalani Nimnanjalee <137691801+nimnamjalee@users.noreply.github.com>
2023-07-07 20:31:47 +0530

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ConvertValues.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class ConvertValues
+    {
+        public void KilometerToMeter()
+        {
+            Console.Write("Enter the distance in kilometers: ");
+            double kilometers = Convert.ToDouble(Console.ReadLine());
+            double meters = kilometers * 1000;
+            Console.WriteLine("The distance in meters is: " + meters);
+            Console.ReadKey();
+        }
+    }
+}
